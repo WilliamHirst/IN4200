1843e7103d271dd20178e9ec03b4c11e84aae0e5
William Hirst <bod_holthe@outlook.com>
2022-03-11 15:53:14 +0100

    Loaded nodes in a array.

--- PageRank/main.c
+++ PageRank/main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <read_grap_from_file.c>
+#include "read_graph_from_file.c"
 
 int main(int argc, char* argv[]){
-    double d, epsilon;
-    int n;
+    //double d, epsilon;
+    //int n;//, *N;
     char *filename;
-    int **row_ptr, **col_idx;
-    double **val;
+    //int **row_ptr, **col_idx;
+    //double **val;
     filename = argv[1];
-    d = atof(argv[2]);
-    epsilon =  atof(argv[3]);
-    n = atioi(argv[4]);
-    read_graph_from_file(char *filename, int *N, int **row_ptr, int **col_idx, double **val)
+    //d = atof(argv[2]);
+    //epsilon =  atof(argv[3]);
+    //n = atoi(argv[4]);
+
+    read_graph_from_file(filename);//, N, row_ptr, col_idx, val);
 }
--- PageRank/read_graph_from_file.c
+++ PageRank/read_graph_from_file.c
@@ -2,9 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 
- void read_graph_from_file(char *filename, int *N, int **row_ptr, int **col_idx, double **val) {
-     FILE *fp;
-     char line [256];
-     fp = fopen(filename, "r");
-     fscanf(fp,"%[^\n]",line);
+ void read_graph_from_file(char *filename){ // int *n, int **row_ptr, int **col_idx, double **val){
+    FILE *fp;
+    int edges, N;
+    char *line  = NULL;
+    size_t size_line = 32;
+    ssize_t reader;
+    fp = fopen(filename, "r");
+    
+    for(int i = 0; i<4; i++){
+        reader = getline(&line, &size_line, fp);
+        if (i == 1) fscanf(fp, " %*s %*s %d %*s %d", &N, &edges);
+    }
+    printf("Number of nodes: %d --- Number of edges: %d\n", N, edges);
+    int tot_edges [edges][2];
+
+    int node_from, node_to, counter;
+    counter = 0;
+    while ((fscanf(fp,"%d %d", &tot_edges[counter][0], &tot_edges[counter][1])) != EOF) {
+        counter += 1;
+    }
+    printf("Nodes and connections loaded.\n");
+
+    fclose (fp);
+   
+
  }
