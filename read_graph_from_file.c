efe7e9d53ded4967310e38d2ea24da0f6fd6572d
William Hirst <bod_holthe@outlook.com>
2022-03-11 11:47:09 +0100

    Create read_graph_from_file.c

---
+++ read_graph_from_file.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+ void read_graph_from_file(char *filename, int *N, int **row_ptr, int **col_idx, double **val) {
+
+ }
+                
